0fbf6f09dca13e4251275fd25fc9f764a933d3ee
Daniel Borrego <daniel.f.borrego@gmail.com>
2023-11-09 14:40:45 +0000

    title

--- FINAL:WhenNothingMatters/script.js
+++ FINAL:WhenNothingMatters/script.js
@@ -1,4 +1,4 @@
-let text = documentlgetElementById('textbox').value;
+//let text = documentlgetElementById('textbox').value;
 
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
@@ -10,119 +10,100 @@ textImage.src = 'font.png';
 const spriteWidth = 48.5;
 const spriteHeight = 65;
 
-const letterSheet = [
-    {
-        letter : a,
-        loc : [0, 0],
-    },
-    {
-        letter : b,
-        loc : [48.5, 0],
-    },
-    {
-        letter : c,
-        loc : [100, 0],
-    },
-    {
-        letter : d,
-        loc : [150, 0],
-    },
-    {
-        letter : e,
-        loc : [200, 0],
-    },
-    {
-        letter : f,
-        loc : [250, 0],
-    },
-    {
-        letter : g,
-        loc : [295, 0],
-    },
-    {
-        letter : h,
-        loc : [350, 0],
-    },
-    {
-        letter : i,
-        loc : [390, 0],
-    },
-    {
-        letter : j,
-        loc : [430, 0],
-    },
-    {
-        letter : k,
-        loc : [475, 0],
-    },
-    {
-        letter : l,
-        loc : [525, 0],
-    },
-    {
-        letter : m,
-        loc : [575, 0],
-    },
-    {
-        letter : n,
-        loc : [635, 0],
-    },
-    {
-         letter : o,
-         loc : [685, 0],
-    },
-    {
-        letter : p,
-        loc : [735, 0],
-    },
-    {
-        letter : q,
-        loc : [785, 0],
-    },
-    {
-        letter : r,
-        loc : [830, 0],
-    },
-    {
-        letter : s,
-        loc : [880, 0],
-    },
-    {
-        letter : t,
-        loc : [930, 0],
-    },
-    {
-        letter : u,
-        loc : [0, 65],
-    },
-    {
-        letter : v,
-        loc : [45, 65],
-    },
-    {
-        letter : w,
-        loc : [99, 65],
-    },
-    {
-        letter : x,
-        loc : [158, 65],
-    },
-    {
-        letter : y,
-        loc : [210, 65],
-    },
-    {
-        letter : z,
-        loc : [260, 65],
+function chooseLoc(letter)
+{
+    switch(letter) {
+        
+        case "a" :
+          return [0, 0]
+          break;
+        case "b" :
+          return [48.5, 0]
+          break;
+        case "c" :
+          return [100, 0]
+          break;
+        case "d" :
+          return [150, 0]
+          break;
+        case "e" :
+          return [200, 0]
+          break;
+        case "f" :
+          return [250, 0]
+          break;
+        case "g" :
+          return [295, 0]
+          break;
+        case "h" :
+          return [350, 0]
+          break;
+        case "i" :
+          return [390, 0]
+          break;
+        case "j" :
+          return [430, 0]
+          break;
+        case "k" :
+          return [475, 0]
+          break;
+        case "l" :
+          return [525, 0]
+          break;
+        case "m" :
+          return [575, 0]
+          break;
+        case "n" :
+          return [635, 0]
+          break;
+        case "o" :
+          return [685, 0]
+          break;
+        case "p" :
+          return [735, 0]
+          break;
+        case "q" :
+          return [785, 0]
+          break;
+        case "r" :
+          return [830, 0]
+          break;
+        case "s" :
+          return [880, 0]
+          break;
+        case "t" :
+          return [930, 0]
+          break;
+        case "u" :
+          return [0, 65]
+          break;
+        case "v" :
+          return [45, 65]
+          break;
+        case "w" :
+          return [99, 65]
+          break;
+        case "x" :
+          return [158, 65]
+          break;
+        case "y" :
+          return [210, 65]
+          break;
+        case "z" :
+          return [260, 65]
+          break;
     }
-];
+}
+
+letterLoc = chooseLoc("k");
 
 
 function drawLetter(){
     ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
     //ctx.fillRect(50,50,100,100);
     //ctx.drawImage(textImage, 0, 0);
-    ctx.drawImage(textImage, z[0], z[1], spriteWidth, 
+    
+    ctx.drawImage(textImage, letterLoc[0], letterLoc[1], spriteWidth, 
         spriteHeight, 0, 0, spriteWidth, spriteHeight);
     
     requestAnimationFrame(drawLetter);
